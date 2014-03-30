7a0beba88c99b46c1ad5cc2a9e353f54d1e24d71
Brandon Closson <bclosson@gmail.com>
2014-03-30 14:58:35 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwidder::Application.config.secret_key_base = '1778026ae6cfb8ac2760e3dafdefc40828542b4bbdbb5f01944c1e0419738f465df3f7b18b311d22598fec660f67cbc1bba9e94607f20a7b4cd7fea57c6ddcc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dwidder::Application.config.session_store :cookie_store, key: '_dwidder_session'
