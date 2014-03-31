40eeb35e0ceefa660c1102308b76fe1a950dac0e
Brandon Closson <bclosson@gmail.com>
2014-03-31 01:31:10 -0400

    Cleaned up tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,8 +30,8 @@ describe "StaticPages" do
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact Us') }
-    it { should have_title(full_title('Contact Us')) }
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 
 end
